3a689fdfda1e0c41fb5ffbb5a965a2476871c9f7
Ravi <ravinj2003@gmail.com>
2026-06-23 00:26:58 +0530

    practice four insert ques and 2 delete ques

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,123 @@
 public class LinkedList {
     Node head;
 
-    
+    // insert at head
+    public void addHead(int d){
+        Node newNode = new Node(d);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // insert at Tail
+    public void addLast(int d){
+        Node newNode = new Node(d);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // insert at index (K-th index)
+    public void insertAtK(int index , int value){
+        int count = 0;
+        if(head == null && index>1){
+            System.out.println("please enter valid index and List is empty");
+            return;
+        }
+        Node newNode = new Node(value);
+        if(index == 1){
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            count++;
+            if(count == index-1){
+                newNode.next = temp.next;
+                temp.next = newNode;
+                return;
+            }
+            temp = temp.next;
+        }
+        System.out.println("Enter valid index (index must be b/w ) 1 and "+(count+1));
+    }
+    // insert before value
+    public void addBeforeVal(int val , int data){
+        if(head == null){
+            System.out.println("list is empty cant able to find value !");
+            return;
+        }
+        if(head.data == val){
+            this.addHead(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        while(temp.next != null){
+            if(temp.next.data == val){
+                newNode.next = temp.next;
+                temp.next = newNode;
+                return;
+            }
+            temp = temp.next;
+        }
+        System.out.println(val+" not existed in List");
+    }
+
+    public void display(){
+        if(head == null){
+            System.out.println("No elements available to Display ");
+            return;
+        }
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    // Deletion at head;
+    public int deleteAtFirst(){
+        if(head == null || head.next==null){
+            System.out.println("first node is empty !");
+            return -1;
+        }
+        int value = head.data;
+        head = head.next;
+        return value;
+    }
+
+    // Deletion at Tail
+    public int deleteAtLast(){
+        int val = -2;
+        if(head == null){
+            System.out.println("list is empty ");
+           
+        }else{
+            Node temp = head;
+            if(temp.next == null){
+                val = head.data;
+                head = null;   
+            }else{
+                while(temp.next.next != null){
+                    temp=temp.next;
+                }
+                val = temp.next.data;
+                temp.next = null;
+            }
+        }
+        return val;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,25 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        LinkedList l = new LinkedList();
+        // l.addLast(11);
+        // l.addLast(13);
+        // l.addLast(15);
+        // l.addHead(7);
+        // l.addLast(18);
+        // l.addHead(1);
+        // l.display();
+        // System.out.println(l.deleteAtFirst());
+        // System.out.println(l.deleteAtLast());
+        // l.display();
+
+        // l.addHead(1);
+        // l.addLast(2);
+        // l.addLast(4);
+        // l.display();
+        // l.addBeforeVal(4, 69);
+        // l.display();
+
+
+        
+    }
 }
